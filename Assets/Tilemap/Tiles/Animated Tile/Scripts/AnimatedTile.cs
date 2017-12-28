2496f0e58a000272faa5af8ae07ef1fea9cd7dc5
ChuanXin-Unity <chuanxin@unity3d.com>
2017-12-28 11:29:53 +0800

    -Update formatting for AnimatedTile.cs

--- Assets/Tilemap/Tiles/Animated Tile/Scripts/AnimatedTile.cs
+++ Assets/Tilemap/Tiles/Animated Tile/Scripts/AnimatedTile.cs
@@ -10,99 +10,99 @@ using UnityEngine;
 
 namespace UnityEngine.Tilemaps
 {
-	[Serializable]
-	public class AnimatedTile : TileBase
-	{
-		public Sprite[] m_AnimatedSprites;
-		public float m_MinSpeed = 1f;
-		public float m_MaxSpeed = 1f;
-		public float m_AnimationStartTime;
-        public Tile.ColliderType m_tileColliderType;
+    [Serializable]
+    public class AnimatedTile : TileBase
+    {
+        public Sprite[] m_AnimatedSprites;
+        public float m_MinSpeed = 1f;
+        public float m_MaxSpeed = 1f;
+        public float m_AnimationStartTime;
+        public Tile.ColliderType m_TileColliderType;
 
         public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData)
-		{
-			tileData.transform = Matrix4x4.identity;
-			tileData.color = Color.white;
-			if (m_AnimatedSprites != null && m_AnimatedSprites.Length > 0)
-			{
-				tileData.sprite = m_AnimatedSprites[m_AnimatedSprites.Length - 1];
-                tileData.colliderType = m_tileColliderType;
+        {
+            tileData.transform = Matrix4x4.identity;
+            tileData.color = Color.white;
+            if (m_AnimatedSprites != null && m_AnimatedSprites.Length > 0)
+            {
+                tileData.sprite = m_AnimatedSprites[m_AnimatedSprites.Length - 1];
+                tileData.colliderType = m_TileColliderType;
             }
-		}
+        }
 
-		public override bool GetTileAnimationData(Vector3Int location, ITilemap tileMap, ref TileAnimationData tileAnimationData)
-		{
-			if (m_AnimatedSprites.Length > 0)
-			{
-				tileAnimationData.animatedSprites = m_AnimatedSprites;
-				tileAnimationData.animationSpeed = Random.Range(m_MinSpeed, m_MaxSpeed);
-				tileAnimationData.animationStartTime = m_AnimationStartTime;
-				return true;
-			}
-			return false;
-		}
+        public override bool GetTileAnimationData(Vector3Int location, ITilemap tileMap, ref TileAnimationData tileAnimationData)
+        {
+            if (m_AnimatedSprites.Length > 0)
+            {
+                tileAnimationData.animatedSprites = m_AnimatedSprites;
+                tileAnimationData.animationSpeed = Random.Range(m_MinSpeed, m_MaxSpeed);
+                tileAnimationData.animationStartTime = m_AnimationStartTime;
+                return true;
+            }
+            return false;
+        }
 
 #if UNITY_EDITOR
-		[MenuItem("Assets/Create/Animated Tile")]
-		public static void CreateAnimatedTile()
-		{
-			string path = EditorUtility.SaveFilePanelInProject("Save Animated Tile", "New Animated Tile", "asset", "Save Animated Tile", "Assets");
-			if (path == "")
-				return;
+        [MenuItem("Assets/Create/Animated Tile")]
+        public static void CreateAnimatedTile()
+        {
+            string path = EditorUtility.SaveFilePanelInProject("Save Animated Tile", "New Animated Tile", "asset", "Save Animated Tile", "Assets");
+            if (path == "")
+                return;
 
-			AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<AnimatedTile>(), path);
-		}
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<AnimatedTile>(), path);
+        }
 #endif
-	}
+    }
 
 #if UNITY_EDITOR
-	[CustomEditor(typeof(AnimatedTile))]
-	public class AnimatedTileEditor : Editor
-	{
-		private AnimatedTile tile { get { return (target as AnimatedTile); } }
+    [CustomEditor(typeof(AnimatedTile))]
+    public class AnimatedTileEditor : Editor
+    {
+        private AnimatedTile tile { get { return (target as AnimatedTile); } }
 
-		public override void OnInspectorGUI()
-		{
-			EditorGUI.BeginChangeCheck();
-			int count = EditorGUILayout.DelayedIntField("Number of Animated Sprites", tile.m_AnimatedSprites != null ? tile.m_AnimatedSprites.Length : 0);
-			if (count < 0)
-				count = 0;
-				
-			if (tile.m_AnimatedSprites == null || tile.m_AnimatedSprites.Length != count)
-			{
-				Array.Resize<Sprite>(ref tile.m_AnimatedSprites, count);
-			}
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            int count = EditorGUILayout.DelayedIntField("Number of Animated Sprites", tile.m_AnimatedSprites != null ? tile.m_AnimatedSprites.Length : 0);
+            if (count < 0)
+                count = 0;
+                
+            if (tile.m_AnimatedSprites == null || tile.m_AnimatedSprites.Length != count)
+            {
+                Array.Resize<Sprite>(ref tile.m_AnimatedSprites, count);
+            }
 
-			if (count == 0)
-				return;
+            if (count == 0)
+                return;
 
-			EditorGUILayout.LabelField("Place sprites shown based on the order of animation.");
-			EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Place sprites shown based on the order of animation.");
+            EditorGUILayout.Space();
 
-			for (int i = 0; i < count; i++)
-			{
-				tile.m_AnimatedSprites[i] = (Sprite) EditorGUILayout.ObjectField("Sprite " + (i+1), tile.m_AnimatedSprites[i], typeof(Sprite), false, null);
-			}
-			
-			float minSpeed = EditorGUILayout.FloatField("Minimum Speed", tile.m_MinSpeed);
-			float maxSpeed = EditorGUILayout.FloatField("Maximum Speed", tile.m_MaxSpeed);
-			if (minSpeed < 0.0f)
-				minSpeed = 0.0f;
-				
-			if (maxSpeed < 0.0f)
-				maxSpeed = 0.0f;
-				
-			if (maxSpeed < minSpeed)
-				maxSpeed = minSpeed;
-			
-			tile.m_MinSpeed = minSpeed;
-			tile.m_MaxSpeed = maxSpeed;
+            for (int i = 0; i < count; i++)
+            {
+                tile.m_AnimatedSprites[i] = (Sprite) EditorGUILayout.ObjectField("Sprite " + (i+1), tile.m_AnimatedSprites[i], typeof(Sprite), false, null);
+            }
+            
+            float minSpeed = EditorGUILayout.FloatField("Minimum Speed", tile.m_MinSpeed);
+            float maxSpeed = EditorGUILayout.FloatField("Maximum Speed", tile.m_MaxSpeed);
+            if (minSpeed < 0.0f)
+                minSpeed = 0.0f;
+                
+            if (maxSpeed < 0.0f)
+                maxSpeed = 0.0f;
+                
+            if (maxSpeed < minSpeed)
+                maxSpeed = minSpeed;
+            
+            tile.m_MinSpeed = minSpeed;
+            tile.m_MaxSpeed = maxSpeed;
 
-			tile.m_AnimationStartTime = EditorGUILayout.FloatField("Start Time", tile.m_AnimationStartTime);
-            tile.m_tileColliderType=(Tile.ColliderType) EditorGUILayout.EnumPopup("Collider Type", tile.m_tileColliderType);
-			if (EditorGUI.EndChangeCheck())
-				EditorUtility.SetDirty(tile);
-		}
-	}
+            tile.m_AnimationStartTime = EditorGUILayout.FloatField("Start Time", tile.m_AnimationStartTime);
+            tile.m_TileColliderType=(Tile.ColliderType) EditorGUILayout.EnumPopup("Collider Type", tile.m_TileColliderType);
+            if (EditorGUI.EndChangeCheck())
+                EditorUtility.SetDirty(tile);
+        }
+    }
 #endif
 }
