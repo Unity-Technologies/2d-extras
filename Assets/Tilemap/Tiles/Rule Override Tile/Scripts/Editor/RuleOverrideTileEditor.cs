50aa3972c4b2265bce20f9bbcd6d0ec3e7bb1819
johnsoncodehk <johnsoncodehk@gmail.com>
2018-05-15 04:08:18 +0800

    Add Rule Override Tile

---
+++ Assets/Tilemap/Tiles/Rule Override Tile/Scripts/Editor/RuleOverrideTileEditor.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Collections.Generic;
+
+namespace UnityEditor
+{
+
+	[CustomEditor(typeof(RuleOverrideTile))]
+	internal class RuleOverrideTileEditor : Editor
+	{
+
+		private List<KeyValuePair<Sprite, Sprite>> m_Sprites;
+
+		ReorderableList m_SpriteList;
+
+		void OnEnable()
+		{
+			RuleOverrideTile ruleOverrideTile = target as RuleOverrideTile;
+
+
+			if (m_Sprites == null)
+				m_Sprites = new List<KeyValuePair<Sprite, Sprite>>();
+
+			if (m_SpriteList == null)
+			{
+				ruleOverrideTile.GetOverrides(m_Sprites);
+
+				m_SpriteList = new ReorderableList(m_Sprites, typeof(KeyValuePair<Sprite, Sprite>), false, true, false, false);
+				m_SpriteList.drawElementCallback = DrawSpriteElement;
+				m_SpriteList.drawHeaderCallback = DrawSpriteHeader;
+				m_SpriteList.onSelectCallback = SelectSprite;
+				m_SpriteList.elementHeight = 64;
+			}
+		}
+
+		void OnDisable()
+		{
+			RuleOverrideTile ruleOverrideTile = target as RuleOverrideTile;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.UpdateIfRequiredOrScript();
+
+			RuleOverrideTile ruleOverrideTile = target as RuleOverrideTile;
+			RuleTile ruleTile = ruleOverrideTile.m_Tile;
+
+			ruleOverrideTile.m_Tile = EditorGUILayout.ObjectField("Tile", ruleTile, typeof(RuleTile), false) as RuleTile;
+
+			using (new EditorGUI.DisabledScope(ruleTile == null))
+			{
+				EditorGUI.BeginChangeCheck();
+				ruleOverrideTile.GetOverrides(m_Sprites);
+
+				m_SpriteList.list = m_Sprites;
+				m_SpriteList.DoLayoutList();
+				if (EditorGUI.EndChangeCheck())
+				{
+					for (int i = 0; i < targets.Length; i++)
+					{
+						RuleOverrideTile tile = targets[i] as RuleOverrideTile;
+						tile.ApplyOverrides(m_Sprites);
+					}
+				}
+			}
+		}
+		private void DrawSpriteElement(Rect rect, int index, bool selected, bool focused)
+		{
+			Sprite originalSprite = m_Sprites[index].Key;
+			Sprite overrideSprite = m_Sprites[index].Value;
+
+			rect.xMax = rect.xMax / 2.0f;
+			GUI.enabled = false;
+			EditorGUI.ObjectField(rect, originalSprite.name, originalSprite, typeof(Sprite), false);
+			GUI.enabled = true;
+			rect.xMin = rect.xMax;
+			rect.xMax *= 2.0f;
+
+			EditorGUI.BeginChangeCheck();
+			overrideSprite = EditorGUI.ObjectField(rect, overrideSprite ? overrideSprite.name : "", overrideSprite, typeof(Sprite), false) as Sprite;
+			if (EditorGUI.EndChangeCheck())
+				m_Sprites[index] = new KeyValuePair<Sprite, Sprite>(originalSprite, overrideSprite);
+		}
+
+		private void DrawSpriteHeader(Rect rect)
+		{
+			rect.xMax = rect.xMax / 2.0f;
+			GUI.Label(rect, "Original", EditorStyles.label);
+			rect.xMin = rect.xMax;
+			rect.xMax *= 2.0f;
+			GUI.Label(rect, "Override", EditorStyles.label);
+		}
+
+		private void SelectSprite(ReorderableList list)
+		{
+			if (0 <= list.index && list.index < m_Sprites.Count)
+				EditorGUIUtility.PingObject(m_Sprites[list.index].Key);
+		}
+	}
+}
---
+++ Assets/Tilemap/Tiles/Rule Override Tile/Scripts/RuleOverrideTile.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Tilemaps;
+
+namespace UnityEngine
+{
+
+	[Serializable]
+	public class TileSpritePair
+	{
+		public Sprite m_OriginalSprite;
+		public Sprite m_OverrideSprite;
+	}
+
+	[Serializable]
+	[CreateAssetMenu]
+	public class RuleOverrideTile : RuleTile
+	{
+
+		public Sprite this[Sprite originalSprite]
+		{
+			get
+			{
+				foreach (TileSpritePair spritePair in m_Sprites)
+				{
+					if (spritePair.m_OriginalSprite == originalSprite)
+					{
+						return spritePair.m_OverrideSprite;
+					}
+				}
+				return null;
+			}
+			set
+			{
+				if (value == null)
+				{
+					m_Sprites = m_Sprites.Where(spritePair => spritePair.m_OriginalSprite != originalSprite).ToList();
+				}
+				else
+				{
+					foreach (TileSpritePair spritePair in m_Sprites)
+					{
+						if (spritePair.m_OriginalSprite == originalSprite)
+						{
+							spritePair.m_OverrideSprite = value;
+							return;
+						}
+					}
+					m_Sprites.Add(new TileSpritePair()
+					{
+						m_OriginalSprite = originalSprite,
+						m_OverrideSprite = value,
+					});
+				}
+			}
+		}
+
+		public RuleTile m_Tile;
+		public List<TileSpritePair> m_Sprites;
+
+		public override void GetTileData(Vector3Int position, ITilemap tileMap, ref TileData tileData)
+		{
+			CopyRules();
+			base.GetTileData(position, tileMap, ref tileData);
+			tileData.sprite = tileData.sprite ? this[tileData.sprite] : null;
+		}
+		public override bool GetTileAnimationData(Vector3Int position, ITilemap tilemap, ref TileAnimationData tileAnimationData)
+		{
+			CopyRules();
+			bool match = base.GetTileAnimationData(position, tilemap, ref tileAnimationData);
+			if (tileAnimationData.animatedSprites != null)
+				tileAnimationData.animatedSprites = tileAnimationData.animatedSprites.Select(sprite => sprite ? this[sprite] : null).ToArray();
+			return match;
+		}
+		public override void RefreshTile(Vector3Int location, ITilemap tileMap)
+		{
+			CopyRules();
+			base.RefreshTile(location, tileMap);
+		}
+
+		public void ApplyOverrides(IList<KeyValuePair<Sprite, Sprite>> overrides)
+		{
+			if (overrides == null)
+				throw new System.ArgumentNullException("overrides");
+
+			for (int i = 0; i < overrides.Count; i++)
+				this[overrides[i].Key] = overrides[i].Value;
+		}
+		public void GetOverrides(List<KeyValuePair<Sprite, Sprite>> overrides)
+		{
+			if (overrides == null)
+				throw new System.ArgumentNullException("overrides");
+
+			overrides.Clear();
+
+			if (!m_Tile)
+				return;
+
+			List<Sprite> originalSprites = new List<Sprite>();
+
+			if (m_Tile.m_DefaultSprite)
+				originalSprites.Add(m_Tile.m_DefaultSprite);
+
+			foreach (RuleTile.TilingRule rule in m_Tile.m_TilingRules)
+				foreach (Sprite sprite in rule.m_Sprites)
+					if (sprite && !originalSprites.Contains(sprite))
+						originalSprites.Add(sprite);
+
+			foreach (Sprite sprite in originalSprites)
+				overrides.Add(new KeyValuePair<Sprite, Sprite>(sprite, this[sprite]));
+		}
+
+		private void CopyRules()
+		{
+			if (!m_Tile)
+				return;
+
+			m_DefaultSprite = m_Tile.m_DefaultSprite;
+			m_DefaultColliderType = m_Tile.m_DefaultColliderType;
+			m_TilingRules = m_Tile.m_TilingRules;
+		}
+	}
+}
