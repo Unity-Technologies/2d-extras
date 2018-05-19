d03f2d620cbbdddd4740a6daf70a89c0c0db4fa0
johnsoncodehk <johnsoncodehk@gmail.com>
2018-05-19 22:12:30 +0800

    Add Rule Base RuleOverrideTile

--- Assets/Tilemap/Tiles/Rule Override Tile/Scripts/RuleOverrideTile.cs
+++ Assets/Tilemap/Tiles/Rule Override Tile/Scripts/RuleOverrideTile.cs
@@ -5,18 +5,22 @@ using UnityEngine.Tilemaps;
 
 namespace UnityEngine
 {
-
-	[Serializable]
-	public class TileSpritePair
-	{
-		public Sprite m_OriginalSprite;
-		public Sprite m_OverrideSprite;
-	}
-
 	[Serializable]
 	[CreateAssetMenu]
 	public class RuleOverrideTile : RuleTile
 	{
+		[Serializable]
+		public class TileSpritePair
+		{
+			public Sprite m_OriginalSprite;
+			public Sprite m_OverrideSprite;
+		}
+		[Serializable]
+		public class OverrideTilingRule
+		{
+			public bool m_Enabled;
+			public TilingRule m_TilingRule = new TilingRule();
+		}
 
 		public Sprite this[Sprite originalSprite]
 		{
@@ -55,28 +59,70 @@ namespace UnityEngine
 				}
 			}
 		}
+		public TilingRule this[TilingRule originalRule]
+		{
+			get
+			{
+				if (!m_Tile)
+					return null;
+
+				int index = m_Tile.m_TilingRules.IndexOf(originalRule);
+				if (index == -1)
+					return null;
+				if (m_OverrideTilingRules.Count < index + 1)
+					return null;
+
+				return m_OverrideTilingRules[index].m_Enabled ? m_OverrideTilingRules[index].m_TilingRule : null;
+			}
+			set
+			{
+				if (!m_Tile)
+					return;
+
+				int index = m_Tile.m_TilingRules.IndexOf(originalRule);
+				if (index == -1)
+					return;
+
+				if (value == null)
+				{
+					if (m_OverrideTilingRules.Count < index + 1)
+						return;
+					m_OverrideTilingRules[index].m_Enabled = false;
+					while (m_OverrideTilingRules.Count > 0 && !m_OverrideTilingRules[m_OverrideTilingRules.Count - 1].m_Enabled)
+						m_OverrideTilingRules.RemoveAt(m_OverrideTilingRules.Count - 1);
+				}
+				else
+				{
+					while (m_OverrideTilingRules.Count < index + 1)
+						m_OverrideTilingRules.Add(new OverrideTilingRule());
+					m_OverrideTilingRules[index].m_Enabled = true;
+					m_OverrideTilingRules[index].m_TilingRule = CloneTilingRule(value);
+					m_OverrideTilingRules[index].m_TilingRule.m_Neighbors = null;
+				}
+			}
+		}
 
 		public RuleTile m_Tile;
+		public bool m_Advanced;
 		public List<TileSpritePair> m_Sprites = new List<TileSpritePair>();
+		public List<OverrideTilingRule> m_OverrideTilingRules = new List<OverrideTilingRule>();
+		public OverrideTilingRule m_OverrideDefault = new OverrideTilingRule();
+		public TilingRule m_OriginalDefault
+		{
+			get
+			{
+				return new TilingRule()
+				{
+					m_Sprites = new Sprite[] { m_Tile != null ? m_Tile.m_DefaultSprite : null },
+					m_ColliderType = m_Tile != null ? m_Tile.m_DefaultColliderType : Tile.ColliderType.None,
+				};
+			}
+		}
 
-		public override void GetTileData(Vector3Int position, ITilemap tileMap, ref TileData tileData)
+		public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
 		{
-			CopyRules();
-			base.GetTileData(position, tileMap, ref tileData);
-			tileData.sprite = tileData.sprite ? this[tileData.sprite] : null;
-		}
-		public override bool GetTileAnimationData(Vector3Int position, ITilemap tilemap, ref TileAnimationData tileAnimationData)
-		{
-			CopyRules();
-			bool match = base.GetTileAnimationData(position, tilemap, ref tileAnimationData);
-			if (tileAnimationData.animatedSprites != null)
-				tileAnimationData.animatedSprites = tileAnimationData.animatedSprites.Select(sprite => sprite ? this[sprite] : null).ToArray();
-			return match;
-		}
-		public override void RefreshTile(Vector3Int location, ITilemap tileMap)
-		{
-			CopyRules();
-			base.RefreshTile(location, tileMap);
+			Override();
+			return base.StartUp(position, tilemap, go);
 		}
 
 		public void ApplyOverrides(IList<KeyValuePair<Sprite, Sprite>> overrides)
@@ -110,15 +156,85 @@ namespace UnityEngine
 			foreach (Sprite sprite in originalSprites)
 				overrides.Add(new KeyValuePair<Sprite, Sprite>(sprite, this[sprite]));
 		}
-
-		private void CopyRules()
+		public void ApplyOverrides(IList<KeyValuePair<TilingRule, TilingRule>> overrides)
 		{
+			if (overrides == null)
+				throw new System.ArgumentNullException("overrides");
+
+			for (int i = 0; i < overrides.Count; i++)
+				this[overrides[i].Key] = overrides[i].Value;
+		}
+		public void GetOverrides(List<KeyValuePair<TilingRule, TilingRule>> overrides)
+		{
+			if (overrides == null)
+				throw new System.ArgumentNullException("overrides");
+
+			overrides.Clear();
+
 			if (!m_Tile)
 				return;
 
-			m_DefaultSprite = m_Tile.m_DefaultSprite;
-			m_DefaultColliderType = m_Tile.m_DefaultColliderType;
-			m_TilingRules = m_Tile.m_TilingRules;
+			foreach (var originalRule in m_Tile.m_TilingRules)
+			{
+				TilingRule overrideRule = this[originalRule];
+				overrides.Add(new KeyValuePair<TilingRule, TilingRule>(originalRule, overrideRule));
+			}
+			overrides.Add(new KeyValuePair<TilingRule, TilingRule>(m_OriginalDefault, m_OverrideDefault.m_TilingRule));
+		}
+
+		public void Override()
+		{
+			if (m_Tile)
+			{
+				m_DefaultSprite = m_Tile.m_DefaultSprite;
+				m_DefaultColliderType = m_Tile.m_DefaultColliderType;
+				m_TilingRules = m_Tile.m_TilingRules.Select(rule => CloneTilingRule(rule)).ToList();
+			}
+			if (!m_Advanced)
+			{
+				if (m_DefaultSprite)
+					m_DefaultSprite = this[m_DefaultSprite];
+				foreach (RuleTile.TilingRule rule in m_TilingRules)
+					for (int i = 0; i < rule.m_Sprites.Length; i++)
+						if (rule.m_Sprites[i])
+							rule.m_Sprites[i] = this[rule.m_Sprites[i]];
+			}
+			else
+			{
+				if (m_OverrideDefault.m_Enabled)
+				{
+					m_DefaultSprite = m_OverrideDefault.m_TilingRule.m_Sprites.Length > 0 ? m_OverrideDefault.m_TilingRule.m_Sprites[0] : null;
+					m_DefaultColliderType = m_OverrideDefault.m_TilingRule.m_ColliderType;
+				}
+				for (int i = 0; i < m_TilingRules.Count; i++)
+				{
+					var originalRule = m_TilingRules[i];
+					TilingRule overrideRule = this[m_Tile.m_TilingRules[i]];
+					if (overrideRule == null)
+						continue;
+					CopyTilingRule(overrideRule, originalRule, false);
+				}
+			}
+		}
+		public TilingRule CloneTilingRule(TilingRule from)
+		{
+			var clone = new TilingRule();
+			CopyTilingRule(from, clone, true);
+			return clone;
+		}
+		public void CopyTilingRule(TilingRule from, TilingRule to, bool copyRule)
+		{
+			if (copyRule)
+			{
+				to.m_Neighbors = from.m_Neighbors;
+				to.m_RuleTransform = from.m_RuleTransform;
+			}
+			to.m_Sprites = from.m_Sprites.Clone() as Sprite[];
+			to.m_AnimationSpeed = from.m_AnimationSpeed;
+			to.m_PerlinScale = from.m_PerlinScale;
+			to.m_Output = from.m_Output;
+			to.m_ColliderType = from.m_ColliderType;
+			to.m_RandomTransform = from.m_RandomTransform;
 		}
 	}
 }
--- Assets/Tilemap/Tiles/Rule Tile/Scripts/Editor/RuleTileEditor.cs
+++ Assets/Tilemap/Tiles/Rule Tile/Scripts/Editor/RuleTileEditor.cs
@@ -14,20 +14,20 @@ namespace UnityEditor
 	[CanEditMultipleObjects]
 	internal class RuleTileEditor : Editor
 	{
-		private const string s_XIconString = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABoSURBVDhPnY3BDcAgDAOZhS14dP1O0x2C/LBEgiNSHvfwyZabmV0jZRUpq2zi6f0DJwdcQOEdwwDLypF0zHLMa9+NQRxkQ+ACOT2STVw/q8eY1346ZlE54sYAhVhSDrjwFymrSFnD2gTZpls2OvFUHAAAAABJRU5ErkJggg==";
-		private const string s_Arrow0 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACYSURBVDhPzZExDoQwDATzE4oU4QXXcgUFj+YxtETwgpMwXuFcwMFSRMVKKwzZcWzhiMg91jtg34XIntkre5EaT7yjjhI9pOD5Mw5k2X/DdUwFr3cQ7Pu23E/BiwXyWSOxrNqx+ewnsayam5OLBtbOGPUM/r93YZL4/dhpR/amwByGFBz170gNChA6w5bQQMqramBTgJ+Z3A58WuWejPCaHQAAAABJRU5ErkJggg==";
-		private const string s_Arrow1 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABqSURBVDhPxYzBDYAgEATpxYcd+PVr0fZ2siZrjmMhFz6STIiDs8XMlpEyi5RkO/d66TcgJUB43JfNBqRkSEYDnYjhbKD5GIUkDqRDwoH3+NgTAw+bL/aoOP4DOgH+iwECEt+IlFmkzGHlAYKAWF9R8zUnAAAAAElFTkSuQmCC";
-		private const string s_Arrow2 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAC0SURBVDhPjVE5EsIwDMxPKFKYF9CagoJH8xhaMskLmEGsjOSRkBzYmU2s9a58TUQUmCH1BWEHweuKP+D8tphrWcAHuIGrjPnPNY8X2+DzEWE+FzrdrkNyg2YGNNfRGlyOaZDJOxBrDhgOowaYW8UW0Vau5ZkFmXbbDr+CzOHKmLinAXMEePyZ9dZkZR+s5QX2O8DY3zZ/sgYcdDqeEVp8516o0QQV1qeMwg6C91toYoLoo+kNt/tpKQEVvFQAAAAASUVORK5CYII=";
-		private const string s_Arrow3 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAB2SURBVDhPzY1LCoAwEEPnLi48gW5d6p31bH5SMhp0Cq0g+CCLxrzRPqMZ2pRqKG4IqzJc7JepTlbRZXYpWTg4RZE1XAso8VHFKNhQuTjKtZvHUNCEMogO4K3BhvMn9wP4EzoPZ3n0AGTW5fiBVzLAAYTP32C2Ay3agtu9V/9PAAAAAElFTkSuQmCC";
-		private const string s_Arrow5 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABqSURBVDhPnY3BCYBADASvFx924NevRdvbyoLBmNuDJQMDGjNxAFhK1DyUQ9fvobCdO+j7+sOKj/uSB+xYHZAxl7IR1wNTXJeVcaAVU+614uWfCT9mVUhknMlxDokd15BYsQrJFHeUQ0+MB5ErsPi/6hO1AAAAAElFTkSuQmCC";
-		private const string s_Arrow6 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACaSURBVDhPxZExEkAwEEVzE4UiTqClUDi0w2hlOIEZsV82xCZmQuPPfFn8t1mirLWf7S5flQOXjd64vCuEKWTKVt+6AayH3tIa7yLg6Qh2FcKFB72jBgJeziA1CMHzeaNHjkfwnAK86f3KUafU2ClHIJSzs/8HHLv09M3SaMCxS7ljw/IYJWzQABOQZ66x4h614ahTCL/WT7BSO51b5Z5hSx88AAAAAElFTkSuQmCC";
-		private const string s_Arrow7 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABQSURBVDhPYxh8QNle/T8U/4MKEQdAmsz2eICx6W530gygr2aQBmSMphkZYxqErAEXxusKfAYQ7XyyNMIAsgEkaYQBkAFkaYQBsjXSGDAwAAD193z4luKPrAAAAABJRU5ErkJggg==";
-		private const string s_Arrow8 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACYSURBVDhPxZE9DoAwCIW9iUOHegJXHRw8tIdx1egJTMSHAeMPaHSR5KVQ+KCkCRF91mdz4VDEWVzXTBgg5U1N5wahjHzXS3iFFVRxAygNVaZxJ6VHGIl2D6oUXP0ijlJuTp724FnID1Lq7uw2QM5+thoKth0N+GGyA7IA3+yM77Ag1e2zkey5gCdAg/h8csy+/89v7E+YkgUntOWeVt2SfAAAAABJRU5ErkJggg==";
-		private const string s_MirrorX = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwQAADsEBuJFr7QAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAAAG1JREFUOE+lj9ENwCAIRB2IFdyRfRiuDSaXAF4MrR9P5eRhHGb2Gxp2oaEjIovTXSrAnPNx6hlgyCZ7o6omOdYOldGIZhAziEmOTSfigLV0RYAB9y9f/7kO8L3WUaQyhCgz0dmCL9CwCw172HgBeyG6oloC8fAAAAAASUVORK5CYII=";
-		private const string s_MirrorY = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwgAADsIBFShKgAAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAAAG9JREFUOE+djckNACEMAykoLdAjHbPyw1IOJ0L7mAejjFlm9hspyd77Kk+kBAjPOXcakJIh6QaKyOE0EB5dSPJAiUmOiL8PMVGxugsP/0OOib8vsY8yYwy6gRyC8CB5QIWgCMKBLgRSkikEUr5h6wOPWfMoCYILdgAAAABJRU5ErkJggg==";
-		private const string s_Rotated = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwQAADsEBuJFr7QAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAAAHdJREFUOE+djssNwCAMQxmIFdgx+2S4Vj4YxWlQgcOT8nuG5u5C732Sd3lfLlmPMR4QhXgrTQaimUlA3EtD+CJlBuQ7aUAUMjEAv9gWCQNEPhHJUkYfZ1kEpcxDzioRzGIlr0Qwi0r+Q5rTgM+AAVcygHgt7+HtBZs/2QVWP8ahAAAAAElFTkSuQmCC";
+		protected const string s_XIconString = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABoSURBVDhPnY3BDcAgDAOZhS14dP1O0x2C/LBEgiNSHvfwyZabmV0jZRUpq2zi6f0DJwdcQOEdwwDLypF0zHLMa9+NQRxkQ+ACOT2STVw/q8eY1346ZlE54sYAhVhSDrjwFymrSFnD2gTZpls2OvFUHAAAAABJRU5ErkJggg==";
+		protected const string s_Arrow0 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACYSURBVDhPzZExDoQwDATzE4oU4QXXcgUFj+YxtETwgpMwXuFcwMFSRMVKKwzZcWzhiMg91jtg34XIntkre5EaT7yjjhI9pOD5Mw5k2X/DdUwFr3cQ7Pu23E/BiwXyWSOxrNqx+ewnsayam5OLBtbOGPUM/r93YZL4/dhpR/amwByGFBz170gNChA6w5bQQMqramBTgJ+Z3A58WuWejPCaHQAAAABJRU5ErkJggg==";
+		protected const string s_Arrow1 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABqSURBVDhPxYzBDYAgEATpxYcd+PVr0fZ2siZrjmMhFz6STIiDs8XMlpEyi5RkO/d66TcgJUB43JfNBqRkSEYDnYjhbKD5GIUkDqRDwoH3+NgTAw+bL/aoOP4DOgH+iwECEt+IlFmkzGHlAYKAWF9R8zUnAAAAAElFTkSuQmCC";
+		protected const string s_Arrow2 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAC0SURBVDhPjVE5EsIwDMxPKFKYF9CagoJH8xhaMskLmEGsjOSRkBzYmU2s9a58TUQUmCH1BWEHweuKP+D8tphrWcAHuIGrjPnPNY8X2+DzEWE+FzrdrkNyg2YGNNfRGlyOaZDJOxBrDhgOowaYW8UW0Vau5ZkFmXbbDr+CzOHKmLinAXMEePyZ9dZkZR+s5QX2O8DY3zZ/sgYcdDqeEVp8516o0QQV1qeMwg6C91toYoLoo+kNt/tpKQEVvFQAAAAASUVORK5CYII=";
+		protected const string s_Arrow3 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAB2SURBVDhPzY1LCoAwEEPnLi48gW5d6p31bH5SMhp0Cq0g+CCLxrzRPqMZ2pRqKG4IqzJc7JepTlbRZXYpWTg4RZE1XAso8VHFKNhQuTjKtZvHUNCEMogO4K3BhvMn9wP4EzoPZ3n0AGTW5fiBVzLAAYTP32C2Ay3agtu9V/9PAAAAAElFTkSuQmCC";
+		protected const string s_Arrow5 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABqSURBVDhPnY3BCYBADASvFx924NevRdvbyoLBmNuDJQMDGjNxAFhK1DyUQ9fvobCdO+j7+sOKj/uSB+xYHZAxl7IR1wNTXJeVcaAVU+614uWfCT9mVUhknMlxDokd15BYsQrJFHeUQ0+MB5ErsPi/6hO1AAAAAElFTkSuQmCC";
+		protected const string s_Arrow6 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACaSURBVDhPxZExEkAwEEVzE4UiTqClUDi0w2hlOIEZsV82xCZmQuPPfFn8t1mirLWf7S5flQOXjd64vCuEKWTKVt+6AayH3tIa7yLg6Qh2FcKFB72jBgJeziA1CMHzeaNHjkfwnAK86f3KUafU2ClHIJSzs/8HHLv09M3SaMCxS7ljw/IYJWzQABOQZ66x4h614ahTCL/WT7BSO51b5Z5hSx88AAAAAElFTkSuQmCC";
+		protected const string s_Arrow7 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABQSURBVDhPYxh8QNle/T8U/4MKEQdAmsz2eICx6W530gygr2aQBmSMphkZYxqErAEXxusKfAYQ7XyyNMIAsgEkaYQBkAFkaYQBsjXSGDAwAAD193z4luKPrAAAAABJRU5ErkJggg==";
+		protected const string s_Arrow8 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACYSURBVDhPxZE9DoAwCIW9iUOHegJXHRw8tIdx1egJTMSHAeMPaHSR5KVQ+KCkCRF91mdz4VDEWVzXTBgg5U1N5wahjHzXS3iFFVRxAygNVaZxJ6VHGIl2D6oUXP0ijlJuTp724FnID1Lq7uw2QM5+thoKth0N+GGyA7IA3+yM77Ag1e2zkey5gCdAg/h8csy+/89v7E+YkgUntOWeVt2SfAAAAABJRU5ErkJggg==";
+		protected const string s_MirrorX = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwQAADsEBuJFr7QAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAAAG1JREFUOE+lj9ENwCAIRB2IFdyRfRiuDSaXAF4MrR9P5eRhHGb2Gxp2oaEjIovTXSrAnPNx6hlgyCZ7o6omOdYOldGIZhAziEmOTSfigLV0RYAB9y9f/7kO8L3WUaQyhCgz0dmCL9CwCw172HgBeyG6oloC8fAAAAAASUVORK5CYII=";
+		protected const string s_MirrorY = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwgAADsIBFShKgAAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAAAG9JREFUOE+djckNACEMAykoLdAjHbPyw1IOJ0L7mAejjFlm9hspyd77Kk+kBAjPOXcakJIh6QaKyOE0EB5dSPJAiUmOiL8PMVGxugsP/0OOib8vsY8yYwy6gRyC8CB5QIWgCMKBLgRSkikEUr5h6wOPWfMoCYILdgAAAABJRU5ErkJggg==";
+		protected const string s_Rotated = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwQAADsEBuJFr7QAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAAAHdJREFUOE+djssNwCAMQxmIFdgx+2S4Vj4YxWlQgcOT8nuG5u5C732Sd3lfLlmPMR4QhXgrTQaimUlA3EtD+CJlBuQ7aUAUMjEAv9gWCQNEPhHJUkYfZ1kEpcxDzioRzGIlr0Qwi0r+Q5rTgM+AAVcygHgt7+HtBZs/2QVWP8ahAAAAAElFTkSuQmCC";
 		
-		private static Texture2D[] s_Arrows;
+		protected static Texture2D[] s_Arrows;
 		public static Texture2D[] arrows
 		{
 			get
@@ -49,7 +49,7 @@ namespace UnityEditor
 			}
 		}
 
-		private static Texture2D[] s_AutoTransforms;
+		protected static Texture2D[] s_AutoTransforms;
 		public static Texture2D[] autoTransforms
 		{
 			get
@@ -65,14 +65,14 @@ namespace UnityEditor
 			}
 		}
 		
-		private ReorderableList m_ReorderableList;
+		protected ReorderableList m_ReorderableList;
 		public RuleTile tile { get { return (target as RuleTile); } }
-		private Rect m_ListRect;
+		protected Rect m_ListRect;
 
-		const float k_DefaultElementHeight = 48f;
-		const float k_PaddingBetweenRules = 13f;
-		const float k_SingleLineHeight = 16f;
-		const float k_LabelWidth = 53f;
+		protected const float k_DefaultElementHeight = 48f;
+		protected const float k_PaddingBetweenRules = 13f;
+		protected const float k_SingleLineHeight = 16f;
+		protected const float k_LabelWidth = 53f;
 			
 		public void OnEnable()
 		{
@@ -86,12 +86,12 @@ namespace UnityEditor
 			m_ReorderableList.onReorderCallback = ListUpdated;
 		}
 
-		private void ListUpdated(ReorderableList list)
+		protected void ListUpdated(ReorderableList list)
 		{
 			SaveTile();
 		}
 
-		private float GetElementHeight(int index)
+		protected float GetElementHeight(int index)
 		{
 			if (tile.m_TilingRules != null && tile.m_TilingRules.Count > 0)
 			{
@@ -106,7 +106,7 @@ namespace UnityEditor
 			return k_DefaultElementHeight + k_PaddingBetweenRules;
 		}
 
-		private void OnDrawElement(Rect rect, int index, bool isactive, bool isfocused)
+		protected void OnDrawElement(Rect rect, int index, bool isactive, bool isfocused)
 		{
 			RuleTile.TilingRule rule = tile.m_TilingRules[index];
 
@@ -126,13 +126,13 @@ namespace UnityEditor
 				SaveTile();
 		}
 
-		private void SaveTile()
+		protected void SaveTile()
 		{
 			EditorUtility.SetDirty(target);
 			SceneView.RepaintAll();
 		}
 
-		private void OnDrawHeader(Rect rect)
+		protected void OnDrawHeader(Rect rect)
 		{
 			GUI.Label(rect, "Tiling Rules");
 		}
@@ -147,7 +147,7 @@ namespace UnityEditor
 				m_ReorderableList.DoLayoutList();
 		}
 
-		private static void RuleMatrixOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
+		protected static void RuleMatrixOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
 		{
 			Handles.color = EditorGUIUtility.isProSkin ? new Color(1f, 1f, 1f, 0.2f) : new Color(0f, 0f, 0f, 0.2f);
 			int index = 0;
@@ -220,13 +220,13 @@ namespace UnityEditor
 			}
 		}
 
-		private static void OnSelect(object userdata)
+		protected static void OnSelect(object userdata)
 		{
 			MenuItemData data = (MenuItemData) userdata;
 			data.m_Rule.m_RuleTransform = data.m_NewValue;
 		}
 
-		private class MenuItemData
+		protected class MenuItemData
 		{
 			public RuleTile.TilingRule m_Rule;
 			public RuleTile.TilingRule.Transform m_NewValue;
@@ -238,12 +238,12 @@ namespace UnityEditor
 			}
 		}
 
-		private void SpriteOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
+		protected void SpriteOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
 		{
 			tilingRule.m_Sprites[0] = EditorGUI.ObjectField(new Rect(rect.xMax - rect.height, rect.yMin, rect.height, rect.height), tilingRule.m_Sprites[0], typeof (Sprite), false) as Sprite;
 		}
 
-		private static void RuleInspectorOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
+		protected static void RuleInspectorOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
 		{
 			float y = rect.yMin;
 			EditorGUI.BeginChangeCheck();
@@ -310,7 +310,7 @@ namespace UnityEditor
 			return base.RenderStaticPreview(assetPath, subAssets, width, height);
 		}
 
-		private static Type GetType(string TypeName)
+		protected static Type GetType(string TypeName)
 		{
 			var type = Type.GetType(TypeName);
 			if (type != null)
@@ -342,7 +342,7 @@ namespace UnityEditor
 			return null;
 		}
 
-		private static Texture2D Base64ToTexture(string base64)
+		protected static Texture2D Base64ToTexture(string base64)
 		{
 			Texture2D t = new Texture2D(1, 1);
 			t.hideFlags = HideFlags.HideAndDontSave;
