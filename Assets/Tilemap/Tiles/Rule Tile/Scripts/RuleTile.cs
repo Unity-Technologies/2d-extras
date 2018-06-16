bef212355bf99987e7e9f713cb0292d62a4d0931
johnsoncodehk <johnsoncodehk@gmail.com>
2018-06-16 11:40:10 +0800

    Customizable RuleTile

--- Assets/Tilemap/Tiles/Rule Tile/Scripts/RuleTile.cs
+++ Assets/Tilemap/Tiles/Rule Tile/Scripts/RuleTile.cs
@@ -4,10 +4,100 @@ using UnityEngine.Tilemaps;
 
 namespace UnityEngine
 {
+	public class RuleTile<T> : RuleTile
+	{
+#if UNITY_EDITOR
+		public override Type m_NeighborType
+		{
+			get { return typeof(T); }
+		}
+		public override void RuleOnGUI(Rect rect, Vector2Int pos, int neighbor)
+		{
+			base.RuleOnGUI(rect, pos, neighbor);
+			var allConsts = typeof(T).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.FlattenHierarchy);
+			foreach (var c in allConsts)
+			{
+				if ((int)c.GetValue(null) == neighbor)
+				{
+					GUI.Label(rect, new GUIContent("", c.Name));
+					break;
+				}
+			}
+		}
+#endif
+	}
 	[Serializable]
 	[CreateAssetMenu]
 	public class RuleTile : TileBase
 	{
+#if UNITY_EDITOR
+		public virtual Type m_NeighborType
+		{
+			get { return typeof(TilingRule.Neighbor); }
+		}
+
+		private const string s_XIconString = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABoSURBVDhPnY3BDcAgDAOZhS14dP1O0x2C/LBEgiNSHvfwyZabmV0jZRUpq2zi6f0DJwdcQOEdwwDLypF0zHLMa9+NQRxkQ+ACOT2STVw/q8eY1346ZlE54sYAhVhSDrjwFymrSFnD2gTZpls2OvFUHAAAAABJRU5ErkJggg==";
+		private const string s_Arrow0 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACYSURBVDhPzZExDoQwDATzE4oU4QXXcgUFj+YxtETwgpMwXuFcwMFSRMVKKwzZcWzhiMg91jtg34XIntkre5EaT7yjjhI9pOD5Mw5k2X/DdUwFr3cQ7Pu23E/BiwXyWSOxrNqx+ewnsayam5OLBtbOGPUM/r93YZL4/dhpR/amwByGFBz170gNChA6w5bQQMqramBTgJ+Z3A58WuWejPCaHQAAAABJRU5ErkJggg==";
+		private const string s_Arrow1 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABqSURBVDhPxYzBDYAgEATpxYcd+PVr0fZ2siZrjmMhFz6STIiDs8XMlpEyi5RkO/d66TcgJUB43JfNBqRkSEYDnYjhbKD5GIUkDqRDwoH3+NgTAw+bL/aoOP4DOgH+iwECEt+IlFmkzGHlAYKAWF9R8zUnAAAAAElFTkSuQmCC";
+		private const string s_Arrow2 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAC0SURBVDhPjVE5EsIwDMxPKFKYF9CagoJH8xhaMskLmEGsjOSRkBzYmU2s9a58TUQUmCH1BWEHweuKP+D8tphrWcAHuIGrjPnPNY8X2+DzEWE+FzrdrkNyg2YGNNfRGlyOaZDJOxBrDhgOowaYW8UW0Vau5ZkFmXbbDr+CzOHKmLinAXMEePyZ9dZkZR+s5QX2O8DY3zZ/sgYcdDqeEVp8516o0QQV1qeMwg6C91toYoLoo+kNt/tpKQEVvFQAAAAASUVORK5CYII=";
+		private const string s_Arrow3 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAB2SURBVDhPzY1LCoAwEEPnLi48gW5d6p31bH5SMhp0Cq0g+CCLxrzRPqMZ2pRqKG4IqzJc7JepTlbRZXYpWTg4RZE1XAso8VHFKNhQuTjKtZvHUNCEMogO4K3BhvMn9wP4EzoPZ3n0AGTW5fiBVzLAAYTP32C2Ay3agtu9V/9PAAAAAElFTkSuQmCC";
+		private const string s_Arrow5 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABqSURBVDhPnY3BCYBADASvFx924NevRdvbyoLBmNuDJQMDGjNxAFhK1DyUQ9fvobCdO+j7+sOKj/uSB+xYHZAxl7IR1wNTXJeVcaAVU+614uWfCT9mVUhknMlxDokd15BYsQrJFHeUQ0+MB5ErsPi/6hO1AAAAAElFTkSuQmCC";
+		private const string s_Arrow6 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACaSURBVDhPxZExEkAwEEVzE4UiTqClUDi0w2hlOIEZsV82xCZmQuPPfFn8t1mirLWf7S5flQOXjd64vCuEKWTKVt+6AayH3tIa7yLg6Qh2FcKFB72jBgJeziA1CMHzeaNHjkfwnAK86f3KUafU2ClHIJSzs/8HHLv09M3SaMCxS7ljw/IYJWzQABOQZ66x4h614ahTCL/WT7BSO51b5Z5hSx88AAAAAElFTkSuQmCC";
+		private const string s_Arrow7 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABQSURBVDhPYxh8QNle/T8U/4MKEQdAmsz2eICx6W530gygr2aQBmSMphkZYxqErAEXxusKfAYQ7XyyNMIAsgEkaYQBkAFkaYQBsjXSGDAwAAD193z4luKPrAAAAABJRU5ErkJggg==";
+		private const string s_Arrow8 = "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAACYSURBVDhPxZE9DoAwCIW9iUOHegJXHRw8tIdx1egJTMSHAeMPaHSR5KVQ+KCkCRF91mdz4VDEWVzXTBgg5U1N5wahjHzXS3iFFVRxAygNVaZxJ6VHGIl2D6oUXP0ijlJuTp724FnID1Lq7uw2QM5+thoKth0N+GGyA7IA3+yM77Ag1e2zkey5gCdAg/h8csy+/89v7E+YkgUntOWeVt2SfAAAAABJRU5ErkJggg==";
+
+		private static Texture2D[] s_Arrows;
+		public static Texture2D[] arrows
+		{
+			get
+			{
+				if (s_Arrows == null)
+				{
+					s_Arrows = new Texture2D[10];
+					s_Arrows[0] = Base64ToTexture(s_Arrow0);
+					s_Arrows[1] = Base64ToTexture(s_Arrow1);
+					s_Arrows[2] = Base64ToTexture(s_Arrow2);
+					s_Arrows[3] = Base64ToTexture(s_Arrow3);
+					s_Arrows[5] = Base64ToTexture(s_Arrow5);
+					s_Arrows[6] = Base64ToTexture(s_Arrow6);
+					s_Arrows[7] = Base64ToTexture(s_Arrow7);
+					s_Arrows[8] = Base64ToTexture(s_Arrow8);
+					s_Arrows[9] = Base64ToTexture(s_XIconString);
+				}
+				return s_Arrows;
+			}
+		}
+
+		public static Texture2D Base64ToTexture(string base64)
+		{
+			Texture2D t = new Texture2D(1, 1);
+			t.hideFlags = HideFlags.HideAndDontSave;
+			t.LoadImage(System.Convert.FromBase64String(base64));
+			return t;
+		}
+
+		public virtual void RuleOnGUI(Rect rect, Vector2Int pos, int neighbor)
+		{
+			switch (neighbor)
+			{
+				case RuleTile.TilingRule.Neighbor.DontCare:
+					break;
+				case RuleTile.TilingRule.Neighbor.This:
+					GUI.DrawTexture(rect, arrows[pos.y * 3 + pos.x]);
+					break;
+				case RuleTile.TilingRule.Neighbor.NotThis:
+					GUI.DrawTexture(rect, arrows[9]);
+					break;
+				default:
+					var style = new GUIStyle();
+					style.alignment = TextAnchor.MiddleCenter;
+					style.fontSize = 10;
+					GUI.Label(rect, neighbor.ToString(), style);
+					break;
+			}
+		}
+#endif
+
 		private static readonly int[,] RotatedOrMirroredIndexes =
 		{
 			{2, 4, 7, 1, 6, 0, 3, 5}, // 90
@@ -20,7 +110,8 @@ namespace UnityEngine
 
 		public Sprite m_DefaultSprite;
 		public Tile.ColliderType m_DefaultColliderType = Tile.ColliderType.Sprite;
-		public TileBase m_Self {
+		public TileBase m_Self
+		{
 			get { return m_OverrideSelf ? m_OverrideSelf : this; }
 			set { m_OverrideSelf = value; }
 		}
@@ -31,7 +122,7 @@ namespace UnityEngine
 		[Serializable]
 		public class TilingRule
 		{
-			public Neighbor[] m_Neighbors;
+			public int[] m_Neighbors;
 			public Sprite[] m_Sprites;
 			public float m_AnimationSpeed;
 			public float m_PerlinScale;
@@ -43,7 +134,7 @@ namespace UnityEngine
 			public TilingRule()
 			{
 				m_Output = OutputSprite.Single;
-				m_Neighbors = new Neighbor[NeighborCount];
+				m_Neighbors = new int[NeighborCount];
 				m_Sprites = new Sprite[1];
 				m_AnimationSpeed = 1f;
 				m_PerlinScale = 0.5f;
@@ -53,8 +144,13 @@ namespace UnityEngine
 					m_Neighbors[i] = Neighbor.DontCare;
 			}
 
+			public class Neighbor
+			{
+				public const int DontCare = 0;
+				public const int This = 1;
+				public const int NotThis = 2;
+			}
 			public enum Transform { Fixed, Rotated, MirrorX, MirrorY }
-			public enum Neighbor { DontCare, This, NotThis }
 			public enum OutputSprite { Single, Random, Animation }
 		}
 
@@ -185,13 +281,23 @@ namespace UnityEngine
 			return original;
 		}
 
+		public virtual bool RuleMatch(int neighbor, TileBase tile)
+		{
+			switch (neighbor)
+			{
+				case TilingRule.Neighbor.This: return tile == m_Self;
+				case TilingRule.Neighbor.NotThis: return tile != m_Self;
+			}
+			return true;
+		}
+
 		public bool RuleMatches(TilingRule rule, ref TileBase[] neighboringTiles, int angle)
 		{
 			for (int i = 0; i < NeighborCount; ++i)
 			{
 				int index = GetRotatedIndex(i, angle);
 				TileBase tile = neighboringTiles[index];
-				if (rule.m_Neighbors[i] == TilingRule.Neighbor.This && tile != m_Self || rule.m_Neighbors[i] == TilingRule.Neighbor.NotThis && tile == m_Self)
+				if (!RuleMatch(rule.m_Neighbors[i], tile))
 				{
 					return false;
 				}
@@ -205,7 +311,7 @@ namespace UnityEngine
 			{
 				int index = GetMirroredIndex(i, mirrorX, mirrorY);
 				TileBase tile = neighboringTiles[index];
-				if (rule.m_Neighbors[i] == TilingRule.Neighbor.This && tile != m_Self || rule.m_Neighbors[i] == TilingRule.Neighbor.NotThis && tile == m_Self)
+				if (!RuleMatch(rule.m_Neighbors[i], tile))
 				{
 					return false;
 				}
