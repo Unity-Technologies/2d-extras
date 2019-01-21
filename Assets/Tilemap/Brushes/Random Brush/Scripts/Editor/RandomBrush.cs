55b2a8eaff52332d0f93204d2d0e8b615981c0ea
ChuanXin-Unity <chuanxin@unity3d.com>
2019-01-21 16:50:53 +0800

    -Add RandomTileSet to RandomBrush

--- Assets/Tilemap/Brushes/Random Brush/Scripts/Editor/RandomBrush.cs
+++ Assets/Tilemap/Brushes/Random Brush/Scripts/Editor/RandomBrush.cs
@@ -8,14 +8,74 @@ namespace UnityEditor
 {
     [CustomGridBrush(false, false, false, "Random Brush")]
     [CreateAssetMenu(fileName = "New Random Brush", menuName = "Brushes/Random Brush")]
-    public class RandomBrush : GridBrush 
+    public class RandomBrush : GridBrush
     {
-        public TileBase[] randomTiles;
+        internal struct SizeEnumerator : IEnumerator<Vector3Int>
+        {
+            private readonly Vector3Int _min, _max, _delta;
+            private Vector3Int _current;
+
+            public SizeEnumerator(Vector3Int min, Vector3Int max, Vector3Int delta)
+            {
+                _min = _current = min;
+                _max = max;
+                _delta = delta;
+                Reset();
+            }
+
+            public SizeEnumerator GetEnumerator()
+            {
+                return this;
+            }
+
+            public bool MoveNext()
+            {
+                if (_current.z >= _max.z)
+                    return false;
+
+                _current.x += _delta.x;
+                if (_current.x >= _max.x)
+                {
+                    _current.x = _min.x;
+                    _current.y += _delta.y;
+                    if (_current.y >= _max.y)
+                    {
+                        _current.y = _min.y;
+                        _current.z = _delta.z;
+                        if (_current.z >= _max.z)
+                            return false;
+                    }
+                }
+                return true;
+            }
+
+            public void Reset()
+            {
+                _current = _min;
+                _current.x -= _delta.x;
+            }
+
+            public Vector3Int Current { get { return _current; } }
+
+            object IEnumerator.Current { get { return Current; } }
+
+            void IDisposable.Dispose() {}
+        }
+
+        [Serializable]
+        public struct RandomTileSet
+        {
+            public TileBase[] randomTiles;
+        }
+
+        public Vector3Int randomTileSetSize = Vector3Int.one;
+        public RandomTileSet[] randomTileSets;
+
         public bool pickRandomTiles;
 
         public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
         {
-            if (randomTiles != null && randomTiles.Length > 0)
+            if (randomTileSets != null && randomTileSets.Length > 0)
             {
                 if (brushTarget == null)
                     return;
@@ -25,11 +85,11 @@ namespace UnityEditor
                     return;
                 
                 Vector3Int min = position - pivot;
-                BoundsInt bounds = new BoundsInt(min, size);
-                foreach (Vector3Int location in bounds.allPositionsWithin)
+                foreach (var startLocation in new SizeEnumerator(min, min + size, randomTileSetSize))
                 {
-                    var randomTile = randomTiles[(int) (randomTiles.Length * UnityEngine.Random.value)];
-                    tilemap.SetTile(location, randomTile);
+                    var randomTileSet = randomTileSets[(int) (randomTileSets.Length * UnityEngine.Random.value)];
+                    var randomBounds = new BoundsInt(startLocation, randomTileSetSize);
+                    tilemap.SetTilesBlock(randomBounds, randomTileSet.randomTiles);
                 }
             }
             else
@@ -38,11 +98,11 @@ namespace UnityEditor
             }
         }
 
-        public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, Vector3Int pickStart)
+        public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt bounds, Vector3Int pickStart)
         {
             if (!pickRandomTiles)
             {
-                base.Pick(gridLayout, brushTarget, position, pickStart);
+                base.Pick(gridLayout, brushTarget, bounds, pickStart);
                 return;
             }
 
@@ -50,17 +110,25 @@ namespace UnityEditor
             if (tilemap == null)
                 return;
 
-            HashSet<TileBase> tileSet = new HashSet<TileBase>();
-            foreach (Vector3Int pos in position.allPositionsWithin)
+            int i = 0;
+            int count = ((bounds.size.x + randomTileSetSize.x - 1) / randomTileSetSize.x)
+                        * ((bounds.size.y + randomTileSetSize.y - 1) / randomTileSetSize.y)
+                        * ((bounds.size.z + randomTileSetSize.z - 1) / randomTileSetSize.z);
+            Array.Resize<RandomTileSet>(ref randomTileSets, count);
+            foreach (var startLocation in new SizeEnumerator(bounds.min, bounds.max, randomTileSetSize))
             {
-                Vector3Int brushPosition = new Vector3Int(pos.x, pos.y, 0);
-                var tile = tilemap.GetTile(brushPosition);
-                if (tile != null)
-                    tileSet.Add(tile);
+                randomTileSets[i].randomTiles = new TileBase[randomTileSetSize.x * randomTileSetSize.y * randomTileSetSize.z];
+                var randomBounds = new BoundsInt(startLocation, randomTileSetSize);
+                int j = 0;
+                foreach (Vector3Int pos in randomBounds.allPositionsWithin)
+                {
+                    var tile = (pos.x < bounds.max.x && pos.y < bounds.max.y && pos.z < bounds.max.z)
+                        ? tilemap.GetTile(pos)
+                        : null;
+                    randomTileSets[i].randomTiles[j++] = tile;
+                }
+                i++;
             }
-
-            Array.Resize<TileBase>(ref randomTiles, tileSet.Count);
-            tileSet.CopyTo(randomTiles);
         }
     }
 
@@ -72,7 +140,7 @@ namespace UnityEditor
         
         public override void PaintPreview(GridLayout grid, GameObject brushTarget, Vector3Int position)
         {
-            if (randomBrush.randomTiles != null && randomBrush.randomTiles.Length > 0)
+            if (randomBrush.randomTileSets != null && randomBrush.randomTileSets.Length > 0)
             {
                 base.PaintPreview(grid, null, position);
                 
@@ -82,15 +150,18 @@ namespace UnityEditor
                 var tilemap = brushTarget.GetComponent<Tilemap>();
                 if (tilemap == null)
                     return;
-                
+
                 Vector3Int min = position - randomBrush.pivot;
-                BoundsInt bounds = new BoundsInt(min, randomBrush.size);
-                foreach (Vector3Int location in bounds.allPositionsWithin)
+                foreach (var startLocation in new RandomBrush.SizeEnumerator(min, min + randomBrush.size, randomBrush.randomTileSetSize))
                 {
-                    var randomTile = randomBrush.randomTiles[(int) (randomBrush.randomTiles.Length * UnityEngine.Random.value)];
-                    tilemap.SetEditorPreviewTile(location, randomTile);
+                    var randomTileSet = randomBrush.randomTileSets[(int) (randomBrush.randomTileSets.Length * UnityEngine.Random.value)];
+                    var randomBounds = new BoundsInt(startLocation, randomBrush.randomTileSetSize);
+                    int j = 0;
+                    foreach (Vector3Int pos in randomBounds.allPositionsWithin)
+                    {
+                        tilemap.SetEditorPreviewTile(pos, randomTileSet.randomTiles[j++]);
+                    }
                 }
-                
                 lastBrushTarget = brushTarget;
             }
             else
@@ -122,23 +193,45 @@ namespace UnityEditor
             EditorGUI.BeginChangeCheck();
             randomBrush.pickRandomTiles = EditorGUILayout.Toggle("Pick Random Tiles", randomBrush.pickRandomTiles);
 
-            int count = EditorGUILayout.DelayedIntField("Number of Tiles", randomBrush.randomTiles != null ? 	randomBrush.randomTiles.Length : 0);
-            if (count < 0)
-                count = 0;
-
-            if (randomBrush.randomTiles == null || randomBrush.randomTiles.Length != count)
+            EditorGUI.BeginChangeCheck();
+            randomBrush.randomTileSetSize = EditorGUILayout.Vector3IntField("Tile Set Size", randomBrush.randomTileSetSize);
+            if (EditorGUI.EndChangeCheck())
             {
-                Array.Resize<TileBase>(ref randomBrush.randomTiles, count);
+                for (int i = 0; i < randomBrush.randomTileSets.Length; ++i)
+                {
+                    int sizeCount = randomBrush.randomTileSetSize.x * randomBrush.randomTileSetSize.y *
+                                    randomBrush.randomTileSetSize.z;
+                    randomBrush.randomTileSets[i].randomTiles = new TileBase[sizeCount];
+                }
+            }
+            int randomTileSetCount = EditorGUILayout.DelayedIntField("Number of Tiles", randomBrush.randomTileSets != null ? randomBrush.randomTileSets.Length : 0);
+            if (randomTileSetCount < 0)
+                randomTileSetCount = 0;
+            if (randomBrush.randomTileSets == null || randomBrush.randomTileSets.Length != randomTileSetCount)
+            {
+                Array.Resize<RandomBrush.RandomTileSet>(ref randomBrush.randomTileSets, randomTileSetCount);
+                for (int i = 0; i < randomBrush.randomTileSets.Length; ++i)
+                {
+                    int sizeCount = randomBrush.randomTileSetSize.x * randomBrush.randomTileSetSize.y *
+                                    randomBrush.randomTileSetSize.z;
+                    if (randomBrush.randomTileSets[i].randomTiles == null
+                        || randomBrush.randomTileSets[i].randomTiles.Length != sizeCount)
+                    randomBrush.randomTileSets[i].randomTiles = new TileBase[sizeCount];
+                }
             }
 
-            if (count > 0)
+            if (randomTileSetCount > 0)
             {
                 EditorGUILayout.Space();
                 EditorGUILayout.LabelField("Place random tiles.");
 
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < randomTileSetCount; i++)
                 {
-                    randomBrush.randomTiles[i] = (TileBase) EditorGUILayout.ObjectField("Tile " + (i+1), randomBrush.randomTiles[i], typeof(TileBase), false, null);
+                    EditorGUILayout.LabelField("Tile Set " + (i+1));
+                    for (int j = 0; j < randomBrush.randomTileSets[i].randomTiles.Length; ++j)
+                    {
+                        randomBrush.randomTileSets[i].randomTiles[j] = (TileBase) EditorGUILayout.ObjectField("Tile " + (j+1), randomBrush.randomTileSets[i].randomTiles[j], typeof(TileBase), false, null);                        
+                    }
                 }
             }
 
