fdb5f8821104d3e8cd6a9e7c0d15dc77ae57f841
johnsoncodehk <johnsoncodehk@gmail.com>
2019-04-23 17:36:22 +0800

    Use the API of Unity 2019 (#90)
    See: https://github.com/Unity-Technologies/2d-extras/issues/89

--- Assets/Tilemap/Tiles/Rule Tile/Scripts/Editor/CustomRuleTileMenu.cs
+++ Assets/Tilemap/Tiles/Rule Tile/Scripts/Editor/CustomRuleTileMenu.cs
@@ -5,14 +5,7 @@
         [MenuItem("Assets/Create/Custom Rule Tile Script", false, 89)]
         static void CreateCustomRuleTile()
         {
-            CreateScriptAsset("Assets/Tilemap/Tiles/Rule Tile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
-        }
-
-        static void CreateScriptAsset(string templatePath, string destName)
-        {
-            typeof(ProjectWindowUtil)
-                .GetMethod("CreateScriptAsset", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)
-                .Invoke(null, new object[] { templatePath, destName });
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Assets/Tilemap/Tiles/Rule Tile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
         }
     }
 }
