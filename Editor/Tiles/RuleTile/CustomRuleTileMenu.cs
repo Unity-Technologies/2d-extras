46477397c56d35f1d1c238c5b3c1ddba3f01b4db
ChuanXin-Unity <chuanxin@unity3d.com>
2019-07-26 15:34:18 +0800

    -Use Package path for New Custom Rule Tile menu item

--- Editor/Tiles/RuleTile/CustomRuleTileMenu.cs
+++ Editor/Tiles/RuleTile/CustomRuleTileMenu.cs
@@ -5,7 +5,7 @@
         [MenuItem("Assets/Create/Custom Rule Tile Script", false, 89)]
         static void CreateCustomRuleTile()
         {
-            ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Assets/Tilemap/Tiles/Rule Tile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Packages/com.unity.2d.tilemap.extras/Editor/Tiles/RuleTile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
         }
     }
 }
