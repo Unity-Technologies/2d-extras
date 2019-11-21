be05b98df143a2f12f06b36030930391df442aac
Slipp Douglas Thompson <git@capnslipp.com>
2019-11-21 08:15:05 -0600

    Fixed instantiatedGameObject to just use the orientationMatrix if no rules match
    * Added a `bool ruleMatched` flag to check if none of the `m_TilingRules` matched— in that case, we set `gameObjectTranslation`/`Rotation`/`Scale` to just the `orientationMatrix` (instead of `(orientationMatrix * transform)`).
    * Minor: Copied the `orientationMatrix` into a `orientMatrix` local instead of calling the accessor repeatedly.

--- Runtime/Tiles/RuleTile/RuleTile.cs
+++ Runtime/Tiles/RuleTile/RuleTile.cs
@@ -282,25 +282,36 @@ namespace UnityEngine
             if (instantiatedGameObject != null)
             {
                 Tilemap tmpMap = tilemap.GetComponent<Tilemap>();
+                Matrix4x4 orientMatrix = tmpMap.orientationMatrix;
                 
                 var iden = Matrix4x4.identity;
                 Vector3 gameObjectTranslation = new Vector3();
                 Quaternion gameObjectRotation = new Quaternion();
                 Vector3 gameObjectScale = new Vector3();
-
+                
+                bool ruleMatched = false;
                 foreach (TilingRule rule in m_TilingRules)
                 {
                     Matrix4x4 transform = iden;
                     if (RuleMatches(rule, location, tilemap, ref transform))
                     {
-                        transform = tmpMap.orientationMatrix * transform;
+                        transform = orientMatrix * transform;
                         
                         // Converts the tile's translation, rotation, & scale matrix to values to be used by the instantiated Game Object
                         gameObjectTranslation = new Vector3(transform.m03, transform.m13, transform.m23);
                         gameObjectRotation = Quaternion.LookRotation(new Vector3(transform.m02, transform.m12, transform.m22), new Vector3(transform.m01, transform.m11, transform.m21));
                         gameObjectScale = transform.lossyScale;
+                        
+                        ruleMatched = true;
                         break;
                     }
+                }
+                if (!ruleMatched)
+                {
+                    // Fallback to just using the orientMatrix for the translation, rotation, & scale values.
+                    gameObjectTranslation = new Vector3(orientMatrix.m03, orientMatrix.m13, orientMatrix.m23);
+                    gameObjectRotation = Quaternion.LookRotation(new Vector3(orientMatrix.m02, orientMatrix.m12, orientMatrix.m22), new Vector3(orientMatrix.m01, orientMatrix.m11, orientMatrix.m21));
+                    gameObjectScale = orientMatrix.lossyScale;
                 }
                 
                 instantiatedGameObject.transform.localPosition = gameObjectTranslation + tmpMap.CellToLocalInterpolated(location + tmpMap.tileAnchor);
