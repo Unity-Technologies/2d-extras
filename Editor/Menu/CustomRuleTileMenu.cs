c2a167e81b281a4b831a496bbc568e03635fb043
ChuanXin-Unity <29473170+ChuanXin-Unity@users.noreply.github.com>
2020-11-26 11:27:46 +0800

    -Create temporary file for Custom Rule Tile Template Script for standalone creation (#247)

--- Editor/Menu/CustomRuleTileMenu.cs
+++ Editor/Menu/CustomRuleTileMenu.cs
@@ -1,13 +1,44 @@
-﻿using UnityEditor.Tilemaps;
+﻿using System;
+using System.IO;
+using UnityEditor.Tilemaps;
+using UnityEngine;
 
 namespace UnityEditor
 {
     static class CustomRuleTileMenu
     {
+        private static string tempCustomRuleTilePath;
+        private const string customRuleTileScript =
+@"using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class #SCRIPTNAME# : RuleTile<#SCRIPTNAME#.Neighbor> {
+    public bool customField;
+
+    public class Neighbor : RuleTile.TilingRule.Neighbor {
+        public const int Null = 3;
+        public const int NotNull = 4;
+    }
+
+    public override bool RuleMatch(int neighbor, TileBase tile) {
+        switch (neighbor) {
+            case Neighbor.Null: return tile == null;
+            case Neighbor.NotNull: return tile != null;
+        }
+        return base.RuleMatch(neighbor, tile);
+    }
+}";
+
         [MenuItem("Assets/Create/2D/Tiles/Custom Rule Tile Script", false, (int)ETilesMenuItemOrder.CustomRuleTile)]
         static void CreateCustomRuleTile()
         {
-            ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Packages/com.unity.2d.tilemap.extras/Editor/Tiles/RuleTile/ScriptTemplates/NewCustomRuleTile.cs.txt", "NewCustomRuleTile.cs");
+            if (String.IsNullOrEmpty(tempCustomRuleTilePath) || !File.Exists(tempCustomRuleTilePath))
+                tempCustomRuleTilePath = FileUtil.GetUniqueTempPathInProject();
+            File.WriteAllText(tempCustomRuleTilePath, customRuleTileScript);
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(tempCustomRuleTilePath, "NewCustomRuleTile.cs");
         }
     }
 }
