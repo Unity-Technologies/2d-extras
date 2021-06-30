bf06aac537a4dff6342be9d6fdfd6b9c11615fca
ChuanXin-Unity <29473170+ChuanXin-Unity@users.noreply.github.com>
2021-06-30 09:30:30 +0800

    Convert cell positions to world positions using the Grid in Tint Brush Smooth (#287)
    * -Convert cell positions to world positions based on the Grid used
    
    * -Remove unused generator
    
    * -Add k_ScaleFactor for better precision when painting non-rectangular Tilemaps
    
    * -Add TintTextureGeneratorEditor

---
+++ Editor/Brushes/TintBrushSmooth/TintTextureGeneratorEditor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityEditor.Tilemaps
+{
+    [CustomEditor(typeof(TintTextureGenerator))]
+    public class TintTextureGeneratorEditor : Editor
+    {
+        private static class Styles
+        {
+            public static GUIContent tintMapSize = new GUIContent("Tint Map Size", "Size of the Tint Map in cells");
+            public static GUIContent scaleFactor = new GUIContent("Scale Factor", "Mapping scale for cells to Tint map texture. Adjust to get better definition.");
+        }
+
+        private const int k_TextureSizeLimit = 4096;
+        
+        private SerializedProperty m_TintMapSize;
+        private SerializedProperty m_ScaleFactor;
+        
+        public void OnEnable()
+        {
+            m_TintMapSize = serializedObject.FindProperty("k_TintMapSize");
+            m_ScaleFactor = serializedObject.FindProperty("k_ScaleFactor");
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.IntSlider(m_TintMapSize, 256, k_TextureSizeLimit, Styles.tintMapSize);
+            EditorGUILayout.IntSlider(m_ScaleFactor, 1, Math.Min(8, Math.Max(1, k_TextureSizeLimit / m_TintMapSize.intValue)), Styles.scaleFactor);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
