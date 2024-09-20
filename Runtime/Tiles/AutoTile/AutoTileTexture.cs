7a1e2ab1896f848f17a7373037a9ad66e778a86e
chuanxin <chuanxin@unity3d.com>
2024-09-20 12:54:50 +0800

    -Remove AutoTileTexture

--- Runtime/Tiles/AutoTile/AutoTileTexture.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UnityEngine.Tilemaps
-{
-    [CreateAssetMenu(fileName = "New AutoTile Texture.att", menuName = "2D/Tiles/AutoTile Texture")]
-    public class AutoTileTexture : ScriptableObject
-    {
-        public Texture2D m_Texture;
-    }
-}
