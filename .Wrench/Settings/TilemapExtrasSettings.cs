92c54316c41c6a614d30abacac25f6a485e38021
chuanxin <>
2025-03-04 14:33:39 +0800

    -Move tests to .tests

--- .Wrench/Settings/TilemapExtrasSettings.cs
+++ .Wrench/Settings/TilemapExtrasSettings.cs
@@ -8,7 +8,7 @@ namespace TilemapExtras.Cookbook.Settings;
 public class TilemapExtrasSettings : AnnotatedSettingsBase
 {
     // Path from the root of the repository where packages are located.
-    readonly string[] PackagesRootPaths = {"."};
+    readonly string[] PackagesRootPaths = {".", ".tests/Packages/com.unity.2d.tilemap.extras.tests"};
 
     // update this to list all packages in this repo that you want to release.
     Dictionary<string, PackageOptions> PackageOptions = new()
@@ -21,18 +21,32 @@ public class TilemapExtrasSettings : AnnotatedSettingsBase
                 {
                     PrePackCommands = new List<Command>()
                     {
-                        new Command("git clone $UNITY_2D_REPO_GIT --no-checkout ../.unity/2d"),
-                        new Command("cd ../.unity/2d && git fetch origin $GIT_BRANCH"),
-                        new Command("cd ../.unity/2d && rm -f .git/index.lock"),
-                        new Command("cd ../.unity/2d && git checkout -f --detach FETCH_HEAD"),
-                        new Command("upm-ci package pack --package-path ../.unity/2d/Packages/com.unity.2d.tilemap.extras.tests"),
-                        new Command("cp upm-ci~/packages/packages.json upm-ci~/packages/com.unity.2d.tilemap.extras.tests_packages.json"),
-                        new Command("rm upm-ci~/packages/packages.json"),
+                        new Command("git clone $UNITY_2D_REPO_GIT --no-checkout .tests"),
+                        new Command("cd .tests && git fetch origin $GIT_BRANCH"),
+                        new Command("cd .tests && rm -f .git/index.lock"),
+                        new Command("cd .tests && git checkout -f --detach FETCH_HEAD"),
                     },
                 },
                 ReleaseOptions = new ReleaseOptions() { IsReleasing = true }
             }
-        }
+        },
+        {
+            "com.unity.2d.tilemap.extras.tests",
+            new PackageOptions()
+            {
+                PackJobOptions = new PackJobOptions()
+                {
+                    PrePackCommands = new List<Command>()
+                    {
+                        new Command("git clone $UNITY_2D_REPO_GIT --no-checkout .tests"),
+                        new Command("cd .tests && git fetch origin $GIT_BRANCH"),
+                        new Command("cd .tests && rm -f .git/index.lock"),
+                        new Command("cd .tests && git checkout -f --detach FETCH_HEAD"),
+                    },
+                },
+                ReleaseOptions = new ReleaseOptions() { IsReleasing = false, NeverPublish = true }
+            }
+        },
     };
 
     public TilemapExtrasSettings()
