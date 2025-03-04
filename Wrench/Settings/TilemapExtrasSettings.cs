5081b4847d4b8d83693b9449bc575810e5ecb1b8
David Teo <david.teo@unity3d.com>
2025-03-04 10:18:28 +0800

    Initial commit for packageWorks in 6000.2

---
+++ Wrench/Settings/TilemapExtrasSettings.cs
@@ -0,0 +1,30 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace TilemapExtras.Cookbook.Settings;
+
+public class TilemapExtrasSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"."};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.unity.2d.tilemap.extras",
+            new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
+        }
+    };
+
+    public TilemapExtrasSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
